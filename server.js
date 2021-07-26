60ea7f00220091cad9aff3310188a7f5ad2f290c
saadishahid <39744515+saadishahid@users.noreply.github.com>
2021-07-26 11:04:07 -0700

    for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 
 const data = require("./client/data");
@@ -112,6 +113,18 @@ app.post("/delete", (req, res) => {
   }, err => err ? console.log(err) : console.log("Deleted Successfully"));
   res.send('deleted successfully');
 });
+
+
+//Serve static assets in production
+
+if (process.env.NODE_ENV === 'production') {
+  //setting up static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
+
 
 app.listen(PORT, () => {
   console.log("server running");
